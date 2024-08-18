b7006a4a90149c62bd63ac437eb08e2bb4605ca5
Osvaldo Abishai Flores Campos <AprendienteAcademico05@gmail.com>
2024-08-17 23:28:53 -0600

    Actualizacion de archivos

--- Databases/Queries/Queries basics.sql
+++ Databases/Queries/Queries basics.sql
@@ -1,14 +1,16 @@
 
 /* Consultas de la tabla Usuario*/
-INSERT INTO usuario(Usuario,Correo,Contraseña,TipoUsuario,Foto)values
+INSERT INTO usuario(Usuario,Correo,Contrasenna,TipoUsuario,Foto)values
 ("AbishaiFC","AprendienteAcademico05@gmail.com","P4s5w0rd!.","Postulante","https://i.pinimg.com/originals/76/f3/f3/76f3f3007969fd3b6db21c744e1ef289.jpg");
 
-Insert into usuario(Usuario,Correo,Contraseña,TipoUsuario,Foto)values
+Insert into usuario(Usuario,Correo,Contrasenna,TipoUsuario,Foto)values
 ("Inclusive Company","InclusiveCompanyOficial@gmail.com","P3RF3C7V1S10N","Empresa","https://i.pinimg.com/originals/eb/1e/4e/eb1e4e321d35811b4da68208f88251dd.jpg");
 
-select Correo,Contraseña from usuario;
+select Correo,Contrasenna from usuario;
 
 Delete from usuario where ID_Usuario = 2 ;
+
+Delete from usuario where ID_Usuario = 3 ;
 
 /* Consultas de la tabla Postulante*/
 insert into postulante
@@ -30,4 +32,36 @@ values
 select Nombre,TipoDeEmpresa,DiscapacidadAdmitidas from empresa;
 
 delete from empresa where ID_Empresa = 2;
+
+/* Consultas de la tabla Perfil Profesional */
+insert into perfilprofesional(ID_Postulante,perfilprofesional,DescripcionPerfilProfesional,Curriculum,IMSS,NivelDeEstudios) 
+values
+("1","Diseñador grafico","Diseñador enfocado a paginas web, marketing y publicidad en general","dvbhufiuewhfiuewfoiwniu","123452","Universidad");
+
+delete from perfilprofesional where id = 2;
+
+/* Consultas de la tabla Discapacidad */
+insert into discapacidad(Id_Postulante,TipoDeDiscapacidad,Dependencias,NiveldeAfectacion,DescripciónAdicional) 
+values ("1","Visual","Lentes","Parcial","La fatiga en la vista es mayor al estar expuesto a cuerpos de luz");
+
+truncate discapacidad;
+
+/* Consultas de la tabla Postulacion */
+insert into postulacion (ID_Postulante,ID_PerfilProfesional,Descripcion) values ("1","1","Busco empleo, facilidad de adaptacion al entorno, Amigable, disponible 24hrs");
+
+truncate postulacion;
+
+/* Consultas de la tabla trabajo */
+
+insert into trabajo
+(ID_Empresa,Categoria,Nombre,Salario,TipoDiscapacidad,Descripcion,Requisitos,TipoDeContrato,DiasLaborales,FechaDePublicacion) 
+values
+("1","Programación","Programador Backend","1000","Del habla, Auditiva, Sensorial","Se solicita un programador minimo de nivel junior, disponibilidad las 24hrs",
+"Disponibilidad, Equipo de computo, conocimientos intermedios de programacion, conocimientos de PHP","Temporal","Lunes,Jueves,Domingo",now());
+
+select Nombre, Salario, TipoDiscapacidad,Requisitos from trabajo;
+
+
+
+
 
